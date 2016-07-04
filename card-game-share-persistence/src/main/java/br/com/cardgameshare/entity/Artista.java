11e42025bfc5510909c2e692c7df65951c2113f0
Lucas Bicudo <ltbicudo@stefanini.com>
2016-07-04 17:10:34 -0300

    Ajustando a entidade Artista para testar a criacao do banco de dados via hibernate

--- card-game-share-persistence/src/main/java/br/com/cardgameshare/entity/Artista.java
+++ card-game-share-persistence/src/main/java/br/com/cardgameshare/entity/Artista.java
@@ -1,9 +1,23 @@
 package br.com.cardgameshare.entity;
 
+import javax.persistence.Column;
+import javax.persistence.Table;
+
 /**
  * Created by Flavia on 7/3/2016.
  */
+@javax.persistence.Entity
+@Table(name = "artista")
 public class Artista extends Entity {
 
+    @Column(name = "nome")
     private String nome;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
