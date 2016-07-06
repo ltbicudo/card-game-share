7357fab2d207aa4b07243fd03b506bfe69571553
ltbicudo <ltbicudo@gmail.com>
2016-07-05 21:25:06 -0300

    Ajuste no RepositoryFactory para criacao correta dos Repositories injetados

--- card-game-share-service/src/main/java/br/com/cardgameshare/service/Service.java
+++ card-game-share-service/src/main/java/br/com/cardgameshare/service/Service.java
@@ -1,14 +1,25 @@
 package br.com.cardgameshare.service;
 
 import br.com.cardgameshare.exception.ExcecaoNegocial;
+import br.com.cardgameshare.repository.RepositoryFactory;
+import br.com.cardgameshare.repository.UsuarioRepository;
+
+import javax.ejb.EJB;
 
 public abstract class Service {
+
+    @EJB
+    private RepositoryFactory repositoryFactory;
 
     protected void tratarExcecaoNaoNegocial(Exception e, String message) throws ExcecaoNegocial {
         if (e instanceof ExcecaoNegocial) {
             throw (ExcecaoNegocial) e;
         }
         throw new ExcecaoNegocial(message, e);
+    }
+
+    protected UsuarioRepository createUsuarioRepository() {
+        return this.repositoryFactory.createUsuarioRepository();
     }
 
 }
