a3fe1ffde7601a5928b70f8a6de90b722228782d
ltbicudo <ltbicudo@gmail.com>
2016-07-09 00:16:48 -0300

    Inclusao da dependencia do Orika

---
+++ card-game-share-common/src/main/java/br/com/cardgameshare/orika/converter/StringConverter.java
@@ -0,0 +1,15 @@
+package br.com.cardgameshare.orika.converter;
+
+import ma.glasnost.orika.CustomConverter;
+import ma.glasnost.orika.metadata.Type;
+
+public class StringConverter extends CustomConverter<String, String> {
+
+    public String convert(String source, Type<? extends String> destinationType) {
+        if (source != null && "".equals(source.trim())) {
+            return null;
+        }
+        return source;
+    }
+
+}
---
+++ card-game-share-persistence/src/main/java/br/com/cardgameshare/orika/factory/TipoContatoFactory.java
@@ -0,0 +1,9 @@
+package br.com.cardgameshare.orika.factory;
+
+import ma.glasnost.orika.ObjectFactory;
+
+public class TipoContatoFactory implements ObjectFactory<TipoContato> {
+
+
+
+}
