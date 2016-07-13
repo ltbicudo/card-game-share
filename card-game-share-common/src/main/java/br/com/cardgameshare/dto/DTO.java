d00974b93a6585df2c02474180d1f8b449ad021b
Lucas Bicudo <ltbicudo@stefanini.com>
2016-07-13 10:51:16 -0300

    Criacao de um DTO pai para conter o ID das entidades

---
+++ card-game-share-common/src/main/java/br/com/cardgameshare/dto/DTO.java
@@ -0,0 +1,14 @@
+package br.com.cardgameshare.dto;
+
+public class DTO {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
