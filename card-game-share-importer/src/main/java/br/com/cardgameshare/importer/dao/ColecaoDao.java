e606e7a1f64aef9e11d3e9eaa2c9b6bd9458333f
Lucas Bicudo <ltbicudo@stefanini.com>
2016-08-15 17:18:11 -0300

    MTGJsonImporter - Melhoria nos nomes de tabelas e queries

--- card-game-share-importer/src/main/java/br/com/cardgameshare/importer/dao/ColecaoDao.java
+++ card-game-share-importer/src/main/java/br/com/cardgameshare/importer/dao/ColecaoDao.java
@@ -10,6 +10,16 @@ import java.util.Date;
 
 public class ColecaoDao {
 
+
+    public static final String TABELA = "COLECAO";
+    public static final String COLUNA_ID = "ID";
+    public static final String COLUNA_NOME = "NOME";
+    public static final String COLUNA_CODIGO = "CODIGO";
+    public static final String COLUNA_DATA_LANCAMENTO = "DATA_LANCAMENTO";
+
+    private static final String SELECT_COMPLETO = "SELECT " + COLUNA_ID + ", " + COLUNA_NOME + ", " + COLUNA_CODIGO + ", " + COLUNA_DATA_LANCAMENTO + " FROM " + TABELA;
+    private static final String INSERT_COMPLETO = "INSERT INTO " + TABELA + "(" + COLUNA_NOME + ", " + COLUNA_CODIGO + ", " + COLUNA_DATA_LANCAMENTO + ") VALUES (?, ?, ?)";
+
     private Connection conn;
 
     public ColecaoDao(Connection conn) {
@@ -19,8 +29,7 @@ public class ColecaoDao {
     public ResultSet buscarPorCodigo(String codigo) {
         try {
             PreparedStatement sql =
-                    conn.prepareStatement(
-                            "SELECT * FROM COLECAO WHERE CODIGO = ?");
+                    conn.prepareStatement(SELECT_COMPLETO + " WHERE " + COLUNA_CODIGO + " = ? ");
             sql.setString(1, codigo);
             JDBC4ResultSet resultadoConsulta = (JDBC4ResultSet) sql.executeQuery();
 
@@ -40,9 +49,7 @@ public class ColecaoDao {
     public void inserir(String nome, String codigo, Date dataLancamento) {
 
         try {
-            PreparedStatement sql =
-                    conn.prepareStatement(
-                            "INSERT INTO COLECAO (NOME, CODIGO, DATA_LANCAMENTO) VALUES (?, ?, ?)");
+            PreparedStatement sql = conn.prepareStatement(INSERT_COMPLETO);
             sql.setString(1, nome);
             sql.setString(2, codigo);
             sql.setDate(3, DateUtil.converterDataUtilEmDataSQL(dataLancamento));
